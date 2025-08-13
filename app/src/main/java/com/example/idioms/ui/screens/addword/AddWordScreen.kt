7eb51fcd2886328ea1b58080f1e89876cd9907f3
Uriel Thoma <urielct2022@gmail.com>
2025-08-13 19:01:45 -0300

    Update AddWordScreen UI

--- app/src/main/java/com/example/idioms/ui/screens/addword/AddWordScreen.kt
+++ app/src/main/java/com/example/idioms/ui/screens/addword/AddWordScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.idioms.ui.screens.addword
 
 import android.widget.Toast
@@ -6,39 +8,53 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
-
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.idioms.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddWordScreen(navigateBack:() -> Unit/*navController: NavController*/) {
+fun AddWordScreen(navBack: () -> Unit) {
     val context = LocalContext.current
 
-    // Datos para las listas desplegables
-    val clases = listOf("Clase 1", "Clase 2", "Clase 3")
-    val idiomas = listOf("Español", "Inglés", "Francés", "Alemán")
+    val categories = listOf("Verbos", "Sustantivos", "Adjetivos")
+    val languages = listOf("Español", "Inglés", "Francés", "Alemán")
 
-    // Variables para los campos de texto
-    var palabra by remember { mutableStateOf("") }
-    var traduccion by remember { mutableStateOf("") }
-    var pronunciacion by remember { mutableStateOf("") }
-    var asociacion by remember { mutableStateOf("") }
-    var claseSeleccionada by remember { mutableStateOf(clases[0]) }
-    var idiomaOrigen by remember { mutableStateOf(idiomas[0]) }
-    var idiomaTraduccion by remember { mutableStateOf(idiomas[1]) }
-    var palabraClaveA by remember { mutableStateOf("") }
-    var palabraClaveB by remember { mutableStateOf("") }
-    var palabraClaveC by remember { mutableStateOf("") }
+    var word by remember { mutableStateOf("") }
+    var translation by remember { mutableStateOf("") }
+    var pronunciation by remember { mutableStateOf("") }
+    var association by remember { mutableStateOf("") }
+    var selectedCategory by remember { mutableStateOf(categories[0]) }
+    var originalLanguage by remember { mutableStateOf(languages[0]) }
+    var translatedLanguage by remember { mutableStateOf(languages[1]) }
+    var keywordA by remember { mutableStateOf("") }
+    var keywordB by remember { mutableStateOf("") }
+    var keywordC by remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
+
+    val imeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+
+    // Si el teclado no está visible, quita el foco
+    LaunchedEffect(imeVisible) {
+        if (!imeVisible) {
+            focusManager.clearFocus()
+        }
+    }
+
 
     Scaffold(
         topBar = {
@@ -46,140 +62,147 @@ fun AddWordScreen(navigateBack:() -> Unit/*navController: NavController*/) {
                 title = {
                     Box(modifier = Modifier.fillMaxWidth()) {
                         IconButton(
-                            onClick = { navigateBack() },
+                            onClick = { navBack() },
                             modifier = Modifier.align(Alignment.CenterStart)
                         ) {
-                            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+                            Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                         }
                         Text(
-                            text = "Agregar palabra",
+                            text = "Add new Word",
                             modifier = Modifier.align(Alignment.Center),
-                            fontSize = 20.sp,
+                            fontSize = dimensionResource(R.dimen.text_size_medium_large).value.sp,
                             fontWeight = FontWeight.Bold,
-                            color = Color.Black
+                            color = MaterialTheme.colorScheme.onBackground
                         )
                     }
                 }
             )
-        },
-        content = { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(5.dp)
-            ) {
-                OutlinedTextField(
-                    value = palabra,
-                    onValueChange = { palabra = it },
-                    label = { Text("Palabra") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = traduccion,
-                    onValueChange = { traduccion = it },
-                    label = { Text("Traducción") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = pronunciacion,
-                    onValueChange = { pronunciacion = it },
-                    label = { Text("Pronunciación") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = asociacion,
-                    onValueChange = { asociacion = it },
-                    label = { Text("Asociación") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                DropdownMenu(
-                    expanded = true,
-                    onDismissRequest = {},
-                    content = {
-                        clases.forEach { clase ->
-                            DropdownMenuItem(onClick = { claseSeleccionada = clase }) {
-                                Text(clase)
-                            }
-                        }
-                    }
-                )
-                OutlinedTextField(
-                    value = claseSeleccionada,
-                    onValueChange = {},
-                    label = { Text("Clase") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                DropdownMenu(
-                    expanded = true,
-                    onDismissRequest = {},
-                    content = {
-                        idiomas.forEach { idioma ->
-                            DropdownMenuItem(onClick = { idiomaOrigen = idioma }) {
-                                Text(idioma)
-                            }
-                        }
-                    }
-                )
-                OutlinedTextField(
-                    value = idiomaOrigen,
-                    onValueChange = {},
-                    label = { Text("Idioma de Origen") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                DropdownMenu(
-                    expanded = true,
-                    onDismissRequest = {},
-                    content = {
-                        idiomas.forEach { idioma ->
-                            DropdownMenuItem(onClick = { idiomaTraduccion = idioma }) {
-                                Text(idioma)
-                            }
-                        }
-                    }
-                )
-                OutlinedTextField(
-                    value = idiomaTraduccion,
-                    onValueChange = {},
-                    label = { Text("Idioma de Traducción") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = palabraClaveA,
-                    onValueChange = { palabraClaveA = it },
-                    label = { Text("Palabra clave A") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = palabraClaveB,
-                    onValueChange = { palabraClaveB = it },
-                    label = { Text("Palabra clave B") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = palabraClaveC,
-                    onValueChange = { palabraClaveC = it },
-                    label = { Text("Palabra clave C") },
-                    modifier = Modifier.fillMaxWidth()
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(dimensionResource(R.dimen.common_padding_default)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.common_padding_mini))
+        ) {
+
+            MyEditText(value = word, label = stringResource(R.string.tf_word)) { word = it }
+
+            MyEditText(value = translation, label = stringResource(R.string.tf_translation)) { translation = it }
+
+            MyEditText(value = pronunciation, label = stringResource(R.string.tf_pronunciation)) { pronunciation = it }
+
+            MyEditText(value = association, label = stringResource(R.string.tf_association)) { association = it }
+
+
+            MyDropdown(
+                label = stringResource(R.string.categories),
+                options = categories,
+                selectedOption = selectedCategory,
+                onOptionSelected = { selectedCategory = it }
+            )
+
+            // Idiomas en fila
+            Row(horizontalArrangement = Arrangement
+                .spacedBy(dimensionResource(R.dimen.common_padding_min)),
+                modifier = Modifier.fillMaxWidth()) {
+
+                MyDropdown(
+                    label = stringResource(R.string.original_language),
+                    options = languages,
+                    selectedOption = originalLanguage,
+                    onOptionSelected = { originalLanguage = it },
+                    modifier = Modifier.weight(1f)
                 )
 
-                Button(
-                    onClick = {
-                        // Agregar lógica para agregar la palabra
-                        Toast.makeText(context, "Palabra agregada", Toast.LENGTH_SHORT).show()
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp)
-                ) {
-                    Text("Agregar Palabra")
-                }
+                MyDropdown(
+                    label = stringResource(R.string.translated_language),
+                    options = languages,
+                    selectedOption = translatedLanguage,
+                    onOptionSelected = { translatedLanguage = it },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            MyEditText(value = keywordA, label = stringResource(R.string.tf_keyword_a)) { keywordA = it }
+            MyEditText(value = keywordB, label = stringResource(R.string.tf_keyword_b)) { keywordB = it }
+            MyEditText(value = keywordC, label = stringResource(R.string.tf_keyword_c)) { keywordC = it }
+
+
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Palabra agregada", Toast.LENGTH_SHORT).show()
+                    navBack()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = dimensionResource(R.dimen.common_padding_default))
+            ) {
+                Text(stringResource(R.string.add_word))
             }
         }
+    }
+}
+
+@Composable
+private fun MyEditText(value:String, label: String,onValueChange: (String) -> Unit ){
+    OutlinedTextField(
+        value = value,
+        onValueChange = { onValueChange( it ) },
+        label = { Text(label) },
+        modifier = Modifier.fillMaxWidth(),
+        maxLines = 1
     )
 }
 
-fun DropdownMenuItem(onClick: () -> Unit, interactionSource: @Composable () -> Unit) {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyDropdown(
+    label: String,
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
 
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            value = selectedOption,
+            onValueChange = {},
+            label = { Text(label) },
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(option) },
+                    onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+
+@Preview
+@Composable
+fun AddWordScreenPreview() {
+    AddWordScreen({})
 }
